83d591fd59bb6e15f985f7effad0f5d416d38212
Halvor Granskogen Bjornstad <halvorgb@gmail.com>
2016-02-04 13:14:12 +0100

    Add 'realistic' ticket release test

---
+++ locustfiles/ticket_release.py
@@ -0,0 +1,64 @@
+import os
+import resource
+import random
+import json
+
+from locust import HttpLocust, TaskSet, task
+from locust.clients import HttpSession
+from locust.exceptions import StopLocust
+
+resource.setrlimit(resource.RLIMIT_NOFILE, (8192, 8192))
+
+reservation_body = """{
+    "tickets": [
+        {"ticket_type_id": 4112466915, "event_id": 1277973654},
+        {"ticket_type_id": 4112466915, "event_id": 1277973654}
+    ]
+}"""
+
+checkout_body = """{
+    "email": "havard+test@hoopla.no",
+    "first_name": "Haavard",
+    "last_name": "Lindset",
+    "data": {
+        "wants_sms": false,
+        "wants_email": false
+    },
+    "return_url": "http://localhost:8765",
+    "checkout_method": "creditcard"
+}"""
+
+class ReservationTaskSet(TaskSet):
+    base_url = os.environ['host']
+
+    @task
+    def create_purchase(self):
+        session = HttpSession(self.base_url)
+        # self.client.post('/api/v0.9/reservation/', reservation_body)
+        reservation_response = session.post('/api/v0.9/reservation/', data=reservation_body)
+
+        if reservation_response.status_code != 201:
+            return
+
+        if reservation_response.status_code == 409:
+            raise StopLocust('No available tickets.')
+
+        checkout_response = session.post('/api/v0.9/reservation/_checkout', data=checkout_body)
+
+        if checkout_response.status_code != 200:
+            return
+
+        purchase = checkout_response.json()['purchase']
+        completion_response = session.post('/api/v0.9/purchases/_complete', json.dumps(dict(
+            purchase_id=purchase['purchase_id'],
+            hash=purchase['completion_params']['hash'],
+            processor=purchase['completion_params']['processor'],
+            payment_ref=purchase['completion_params']['order_ref']
+        )))
+
+
+class ReservationLocust(HttpLocust):
+    host = os.environ['host']
+    task_set = ReservationTaskSet
+    min_wait = 0
+    max_wait = 0
